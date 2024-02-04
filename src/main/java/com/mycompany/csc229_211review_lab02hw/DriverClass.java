9fdf43362adbc745bcad3cca62be2f010e65ac47
stevenlin <stevenlin1010@gmail.com>
2024-02-04 04:46:48 -0500

    I used "//" to explain my code.

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -4,25 +4,20 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
+import java.util.Scanner;
 public class DriverClass {
-	
-	public static void main(String[] args) {
-		// ToDo 5: Fix the error
-		
-		// ToDo 6: Fix the constructor of Student class
-		
-		// ToDo 7: Add a toString method for Student class
-		
-		Student std1= new Student("James", 20);
-		
-		// ToDo 8: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
-		
-		System.out.println(std1);
-		
-		// ToDo 9: add comments and explain your code
-		
-		// ToDo 10: submit using a pull request.
-	}
 
+	public static void main(String[] args) {
+
+		Student std1= new Student("Steven", (short) 19);
+
+		// Create a Scanner object to get user input
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter GPA: ");
+		// Get the GPA from user input and set it for the student
+		double gpa = scanner.nextDouble();
+		std1.setGpa(gpa);
+
+		System.out.println(std1);
+	}
 }
--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -4,13 +4,41 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Person
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+public class Student extends Person {
 
+    // Field for GPA
+    private double gpa;
+
+    // Constructor for Student class
+    public Student(String name, short age) {
+        super(name, age);
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    // Abstract methods for address
+    @Override
+    public String getAddress() {
+        return null;
+    }
+
+    @Override
+    public void setAddress(String address) {
+    }
+
+    // Overridden toString method to print the student details
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                ", gpa=" + gpa +
+                '}';
+    }
 }
